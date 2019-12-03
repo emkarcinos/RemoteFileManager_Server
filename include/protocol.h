5d6bc33fff59db6ddd74cffcffbde0516bc1e07c
Marcin Kostrzewski <Komar86311@gmail.com>
2019-12-03 18:54:31 +0100

    Defined a protocol

---
+++ include/protocol.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/* CLIENT MESSAGES */
+
+/* Asks for directory list */
+#define C_DIR   39235sendmedir
+/* Asks for a file number */
+#define C_FILE  62384filenumber
+/* Tells goodbye */
+#define C_STOP  34569goodbye
+
+/* SERVER MESSAGES */
+
+/* Begin Directory list stream */
+#define DIR_START   48375dirstart
+/* End Directory list stream */
+#define DIR_DONE    35236dirdone
+/* Begin File stream */
+#define FILE_START  83568filestart
+/* End File stream */
+#define FILE_DONE   59837filedone
+/* File Error */
+#define FILE_ERR    14987fileerr
+/* Dir Error */
+#define DIR_ERR     39562direrr
