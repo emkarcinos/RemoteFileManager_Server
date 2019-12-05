00d3bb05636b5284df1f35ce0e7fb0f6d53c45bd
Marcin Kostrzewski <Komar86311@gmail.com>
2019-12-05 18:47:49 +0100

    Added packet sending on redefined protocol

--- include/io.h
+++ include/io.h
@@ -15,3 +15,10 @@ void sendDirectory(int sockfd, const struct File_d** dirTable);
 void sendFileSize(int sockfd, FILE* filePtr);
 
 char* composePacket(char type, int len, const char* message);
+
+int getPacketSize(const char* packet);
+
+void sendPacket(int sockfd, const char* packet);
+
+void sendString(int sockfd, const char* message);
+
--- src/io.c
+++ src/io.c
@@ -8,6 +8,7 @@
 #include "log.h"
 #include "protocol.h"
 
+/* deprecated */
 char *getMessage(const int socket) {
     static char msg[BUF_SIZE];
     memset(msg, '\0', BUF_SIZE * sizeof(char));
@@ -91,4 +92,27 @@ char* composePacket(const char type, const int len, const char* message){
     memcpy(finalMessage+5, message, len);
 
     return finalMessage;
+}
+
+int getPacketSize(const char* packet){
+    int lenEndian = 0;
+    memcpy(lenEndian, packet+1, 4);
+    int result = ntohl(lenEndian);
+    return result;
+}
+
+void sendPacket(int sockfd, const char* packet){
+    int len = getPacketSize(packet);
+    log_info("Sending a packet of length %d.", len);
+    int sentBytes = send(sockfd, packet, len, 0);
+    if (sentBytes <= 0)
+        log_error("An error has occurred during sending.");
+    else
+        log_info("Packet sent.");
+}
+
+void sendString(int sockfd, const char* message){
+    const int len = strlen(message);
+    const char* packet = composePacket(STR, len, message);
+    sendPacket(sockfd, packet);
 }
