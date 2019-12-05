f605ac20cba531bdaddbd7a0f08bf7baf06453e5
Marcin Kostrzewski <Komar86311@gmail.com>
2019-12-05 18:36:58 +0100

    Redefined the protocol

--- include/protocol.h
+++ include/protocol.h
@@ -1,31 +1,34 @@
 #pragma once
 
+/* This file specifies the type of packets */
+
 /* CLIENT MESSAGES */
 
 /* Allows for data transfer */
-#define C_ACK   "33986ack"
+#define C_ACK   'a'
 /* Asks for directory list */
-#define C_DIR   "39235sendmedir"
+#define C_DIR   'd'
 /* Asks for a file number */
-#define C_FILE  "62384filenumber"
+#define C_FILE  'f'
 /* Yes */
-#define C_YES   "12385yesans"
+#define C_YES   'y'
 /* No */
-#define C_NO    "98632noans"
+#define C_NO    'n'
 /* Tells goodbye */
-#define C_STOP  "34569goodbye"
+#define C_STOP  's'
 
 /* SERVER MESSAGES */
 
+#define STR         '1'
 /* Begin Directory list stream */
-#define DIR_START   "48375dirstart"
+#define DIR_START   '2'
 /* End Directory list stream */
-#define DIR_DONE    "35236dirdone"
+#define DIR_DONE    '3'
 /* Begin File stream */
-#define FILE_START  "83568filestart"
+#define FILE_START  '4'
 /* End File stream */
-#define FILE_DONE   "59837filedone"
+#define FILE_DONE   '5'
 /* File Error */
-#define FILE_ERR    "14987fileerr"
+#define FILE_ERR    '6'
 /* Dir Error */
-#define DIR_ERR     "39562direrr"
+#define DIR_ERR     '7'
